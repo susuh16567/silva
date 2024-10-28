ad620bdac775196300a31fea61800dd3ffec55a1
susuh16567 <146451163+susuh16567@users.noreply.github.com>
2024-10-28 11:47:54 -0300

    ppko

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+'Programação',
+'O que é Python?',
+'O Python é uma linguagem de programação'
+
+)
+
+criaCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris'
+
+)
+
+criaCartao(
+'Lingua Inglesa',
+'Como se diz oi em inglễ?',
+'Oi em inglễs é HI (RAI)'
+
+)
